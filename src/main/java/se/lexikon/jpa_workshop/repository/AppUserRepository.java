e383f64bd3c4590100fc61c97c7cbfe0063f89cb
Hernan Leiva <hleiva@hotmail.com>
2025-04-09 11:09:23 +0200

    part 2. App user, details requirements, AppUserRepository, DetailsRepository requirements done

--- src/main/java/se/lexikon/jpa_workshop/repository/AppUserRepository.java
+++ src/main/java/se/lexikon/jpa_workshop/repository/AppUserRepository.java
@@ -3,8 +3,15 @@ package se.lexikon.jpa_workshop.repository;
 import org.springframework.data.repository.CrudRepository;
 import se.lexikon.jpa_workshop.entity.AppUser;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface AppUserRepository extends CrudRepository<AppUser, Integer> {
 
-
-
+    AppUser findByUsername(String username);
+    LocalDate startDate = LocalDate.of(2025,2,1);
+    LocalDate endDate = LocalDate.of(2025,3,1);
+    List<AppUser> findByRegDateBetween(LocalDate startDate, LocalDate endDate);
+    List<AppUser> findByUserDetails_Id(int id);
+    AppUser findByemailIgnoreCase(String email);
 }
--- src/main/java/se/lexikon/jpa_workshop/repository/DetailsRepository.java
+++ src/main/java/se/lexikon/jpa_workshop/repository/DetailsRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import se.lexikon.jpa_workshop.entity.Details;
 
+import java.util.List;
+
 public interface DetailsRepository extends CrudRepository<Details, Integer> {
 
-
-
+    Details findByemail(String email);
+    List<Details> findAllByNameContains(String name);
+    Details findByNameIgnoreCase(String name);
 }
