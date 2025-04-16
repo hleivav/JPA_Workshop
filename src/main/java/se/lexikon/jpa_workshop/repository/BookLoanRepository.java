230b53117c90c663025f6d519940389df526b78b
Hernan Leiva <hleiva@hotmail.com>
2025-04-16 12:00:04 +0200

    Part 3 completed

--- src/main/java/se/lexikon/jpa_workshop/repository/BookLoanRepository.java
+++ src/main/java/se/lexikon/jpa_workshop/repository/BookLoanRepository.java
@@ -1,8 +1,24 @@
 package se.lexikon.jpa_workshop.repository;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import se.lexikon.jpa_workshop.entity.BookLoan;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface BookLoanRepository extends CrudRepository<BookLoan, Integer> {
+
+    BookLoan findByBorrowerId(int borrowerId);
+    BookLoan findByBookId(int bookId);
+    List<BookLoan> findAllByReturned(boolean returned);
+    @Query(value = "select bl from BookLoan bl where bl.dueDate < CURRENT DATE ")
+    List<BookLoan> selectOverdueBookLoans();
+    List<BookLoan> findAllByLoanDateBetween(LocalDate startDate, LocalDate endDate);
+    @Modifying
+    @Query(value = "update BookLoan bl set bl.returned = true where bl.id = :id")
+    int markABookAsReturnedByLoanId(@Param("id")int loanId);
 
 }
--- src/main/java/se/lexikon/jpa_workshop/repository/BookRepository.java
+++ src/main/java/se/lexikon/jpa_workshop/repository/BookRepository.java
@@ -3,8 +3,12 @@ package se.lexikon.jpa_workshop.repository;
 import org.springframework.data.repository.CrudRepository;
 import se.lexikon.jpa_workshop.entity.Book;
 
+import java.util.List;
+
 public interface BookRepository extends CrudRepository<Book, Integer> {
 
-
+    Book findByIsbn(String isbn);
+    List<Book> findAllByTitleContains(String title);
+    List<Book> findAllByMaxLoanDaysLessThan(int maxLoanDays);
 
 }
